3c052159b7aa86017b5a7d090c7d5612da82d0e4
Bjørn Arve Lagim <bjornarvelagim@MacBookWIFI.lan>
2017-11-06 06:59:15 +0100

    Image Paths corrected for all images + fix. Added all pages needed for admin loging/admin creation/list of administrable calendars and editor page with tabs.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -33,6 +33,14 @@ export class LoginPage {
   viewMain(){
     this.navCtrl.push('MainPage');
   }
+
+  createNewAdmin(){
+    this.navCtrl.push('CalendarCreatePage');
+  }
+
+  adminLogin(){
+    this.navCtrl.push('CalendarAdministratePage');
+  }
 }
 
 // TODO: Clean up presentation on login and calendar screens. Images and stylings. Door sizes.
